3976f821e25fde79d245009cfa8718821d5d1e71
Crowdbotics <engineering@crowdbotics.com>
2023-05-27 23:17:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/delicate_limit_dev_86553/MainActivity.java
@@ -0,0 +1,15 @@
+package com.delicate_limit_dev_86553;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "delicate_limit_dev_86553";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-limit-dev-86553",
+  url: "https://delicate-limit-dev-86553.botics.co",
+  api: "https://delicate-limit-dev-86553.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
